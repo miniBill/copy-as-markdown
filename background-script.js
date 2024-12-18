73ae519261c3c859f494098bf361f3067913c36f
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2024-12-18 03:05:31 +0100

    Add support for `em`

--- background-script.js
+++ background-script.js
@@ -1,23 +1,23 @@
 const browser = chrome || window["browser"];
 
 async function getCurrentTab() {
-  // https://developer.chrome.com/docs/extensions/reference/api/tabs
-  let queryOptions = { active: true, lastFocusedWindow: true };
-  // `tab` will either be a `tabs.Tab` instance or `undefined`.
-  let [tab] = await browser.tabs.query(queryOptions);
-  return tab;
+    // https://developer.chrome.com/docs/extensions/reference/api/tabs
+    let queryOptions = { active: true, lastFocusedWindow: true };
+    // `tab` will either be a `tabs.Tab` instance or `undefined`.
+    let [tab] = await browser.tabs.query(queryOptions);
+    return tab;
 }
 
 function getText() {
-  // https://stackoverflow.com/a/40087980/1181553
-  var range = window.getSelection().getRangeAt(0); // Get the selected range
-  var div = document.createElement("div");
-  div.appendChild(range.cloneContents()); // Get the document fragment from selected range
-  return div.innerHTML; // Return the actual HTML
+    // https://stackoverflow.com/a/40087980/1181553
+    var range = window.getSelection().getRangeAt(0); // Get the selected range
+    var div = document.createElement("div");
+    div.appendChild(range.cloneContents()); // Get the document fragment from selected range
+    return div.innerHTML; // Return the actual HTML
 }
 
 async function writeToClipboard(text) {
-  await navigator.clipboard.writeText(text);
+    await navigator.clipboard.writeText(text);
 }
 
 /**
@@ -25,82 +25,83 @@ async function writeToClipboard(text) {
  * @returns {string}
  */
 function toMarkdown(text) {
-  String.prototype.__replaceTag = function (tag, replacement) {
-    return this.replaceAll(`<${tag}> `, ` ${replacement}`)
-      .replaceAll(`<${tag}>`, replacement)
-      .replaceAll(` </${tag}>`, `${replacement} `)
-      .replaceAll(`</${tag}>`, replacement);
-  };
+    String.prototype.__replaceTag = function (tag, replacement) {
+        return this.replaceAll(`<${tag}> `, ` ${replacement}`)
+            .replaceAll(`<${tag}>`, replacement)
+            .replaceAll(` </${tag}>`, `${replacement} `)
+            .replaceAll(`</${tag}>`, replacement);
+    };
 
-  return text
-    .replaceAll("</p>\n<p>", "\n> \n> ")
-    .replaceAll(
-      /<span style="text-decoration: underline;">([^<]*)<\/span>/g,
-      (_, inner) => `__${inner}__`
-    )
-    .replaceAll(
-      /<span style="text-decoration: line-through;">([^<]*)<\/span>/g,
-      (_, inner) => `~~${inner}~~`
-    )
-    .replaceAll(
-      /<span style="color: #[0-9a-f]*;">([^<]*)<\/span>/g,
-      (_, inner) => inner
-    )
-    .replaceAll(
-      /<a(?: title="[^"]*")? href="([^"]*)"(?: [a-z]*="[^"]*")*>([^<]*)<\/a>/g,
-      (_, href, label) => `[${label}](${href})`
-    )
-    .replaceAll("&nbsp;", " ")
-    .replaceAll("<br>", "\n> ")
-    .__replaceTag("p", "")
-    .__replaceTag("em", "_")
-    .__replaceTag("s", "~~")
-    .__replaceTag("i", "_")
-    .__replaceTag("strong", "**")
-    .__replaceTag("b", "**")
-    .replaceAll(/^-/g, () => "\\-");
+    return text
+        .replaceAll("</p>\n<p>", "\n> \n> ")
+        .replaceAll(
+            /<span style="text-decoration: underline;">([^<]*)<\/span>/g,
+            (_, inner) => `__${inner}__`
+        )
+        .replaceAll(
+            /<span style="text-decoration: line-through;">([^<]*)<\/span>/g,
+            (_, inner) => `~~${inner}~~`
+        )
+        .replaceAll(
+            /<span style="color: #[0-9a-f]*;">([^<]*)<\/span>/g,
+            (_, inner) => inner
+        )
+        .replaceAll(
+            /<a(?: title="[^"]*")? href="([^"]*)"(?: [a-z]*="[^"]*")*>([^<]*)<\/a>/g,
+            (_, href, label) => `[${label}](${href})`
+        )
+        .replaceAll("&nbsp;", " ")
+        .replaceAll("<br>", "\n> ")
+        .__replaceTag("p", "")
+        .__replaceTag("em", "_")
+        .__replaceTag("s", "~~")
+        .__replaceTag("i", "_")
+        .__replaceTag("em", "_")
+        .__replaceTag("strong", "**")
+        .__replaceTag("b", "**")
+        .replaceAll(/^-/g, () => "\\-");
 }
 
 /** @param {chrome.tabs.Tab | undefined} tab  */
 async function formattedCopy(tab) {
-  if (!tab) {
-    tab = await getCurrentTab();
-  }
-  const [{ result }] = await browser.scripting.executeScript({
-    target: { tabId: tab.id },
-    func: getText,
-  });
-  const formatted = `> ${toMarkdown(result.trim())}\n`;
-  await browser.scripting.executeScript({
-    target: { tabId: tab.id },
-    func: writeToClipboard,
-    args: [formatted],
-  });
-  console.log(formatted);
+    if (!tab) {
+        tab = await getCurrentTab();
+    }
+    const [{ result }] = await browser.scripting.executeScript({
+        target: { tabId: tab.id },
+        func: getText,
+    });
+    const formatted = `> ${toMarkdown(result.trim())}\n`;
+    await browser.scripting.executeScript({
+        target: { tabId: tab.id },
+        func: writeToClipboard,
+        args: [formatted],
+    });
+    console.log(formatted);
 }
 
 browser.runtime.onInstalled.addListener(() => {
-  console.clear();
+    console.clear();
 
-  browser.contextMenus.create({
-    id: "copy-as-markdown",
-    title: "Copy as Markdown",
-    contexts: ["selection"],
-  });
+    browser.contextMenus.create({
+        id: "copy-as-markdown",
+        title: "Copy as Markdown",
+        contexts: ["selection"],
+    });
 });
 
 browser.commands.onCommand.addListener(async (command, tab) => {
-  switch (command) {
-    case "copy-as-markdown":
-      await formattedCopy(tab);
-      break;
-  }
+    switch (command) {
+        case "copy-as-markdown":
+            await formattedCopy(tab);
+            break;
+    }
 });
 
 browser.contextMenus.onClicked.addListener(async (info, tab) => {
-  switch (info.menuItemId) {
-    case "copy-as-markdown":
-      await formattedCopy(tab);
-      break;
-  }
+    switch (info.menuItemId) {
+        case "copy-as-markdown":
+            await formattedCopy(tab);
+            break;
+    }
 });
